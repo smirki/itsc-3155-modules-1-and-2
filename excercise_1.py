da11fe8dfe9ca2e8462e265c660aa13c05ab606c
Manav Majumdar <kookygavinquack@gmail.com>
2022-01-25 19:20:38 -0500

    First excercise

---
+++ excercise_1.py
@@ -0,0 +1,7 @@
+x = input("Enter your string: ")
+
+
+# Just in case
+# object[<start>:<stop>:<step>], x is a string object, which follows the pattern.
+# it starts at the end and ends at the beginning, and goes to it with the step function as a -1.
+print(x[::-1]) 
